e5509036f4fa155f54db79f1bad277682d8739f9
Alwan7 <alilewli0@gmail.com>
2021-12-28 18:37:32 +0100

    fetch api

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': 'fa8320bca0msh4798320bfc85522p17ee07jsn34f347f4ab0b'
+
+}
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCrypto: builder.query({
+            query: () => createRequest('/exchanges')
+        })
+    })
+})
