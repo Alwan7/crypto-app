64018cef9c6ce41758b21760118e3bce2d1a1ccb
Alwan7 <alilewli0@gmail.com>
2022-01-10 10:49:30 +0100

    change port

---
+++ backend/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static(path.join(__dirname, '../dist')));
+app.listen(3010, () => console.log('Listening on port 3010'));
